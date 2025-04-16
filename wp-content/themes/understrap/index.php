524806e54857e6a9937b2fa611307fb67a867d97
Andre <andre_ranulfo@hotmail.com>
2025-04-16 16:32:46 -0300

    login form working

--- wp-content/themes/understrap/index.php
+++ wp-content/themes/understrap/index.php
@@ -13,7 +13,25 @@ if (is_front_page()) {
 		<div class="col-4">3</div>
 		<div class="login-wp">
 			<?php 
-			wp_login_form();
+			$args = array(
+				'echo' => true, 
+				'redirect' => home_url(), // Redirect to the homepage after login
+				'form_id' => 'loginform',
+				'label_username' => __('Username or Email Address'),
+				'label_password' => __('Password'),
+				'label_remember' => __('Remember Me'),
+				'label_log_in' => __('Log In'),
+				'id_username' => 'user_login',
+				'id_password' => 'user_pass',
+				'id_remember' => 'rememberme',
+				'id_submit' => 'wp-submit',
+				'remember' => true,
+				'login_button' => true,
+				'value_username' => '',
+				'value_remember' => false,
+			);
+
+			wp_login_form($args);
 			?>
 
 		</div>
