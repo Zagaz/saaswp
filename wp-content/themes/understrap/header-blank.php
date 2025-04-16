b54503b23f9e77d6732599e97b23165a714863a8
Andre <andre_ranulfo@hotmail.com>
2025-04-16 16:17:29 -0300

    login form

---
+++ wp-content/themes/understrap/header-blank.php
@@ -0,0 +1,19 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+$bootstrap_version = get_theme_mod( 'understrap_bootstrap_version', 'bootstrap4' );
+$navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class('body-full'); ?> >
+<?php do_action( 'wp_body_open' ); ?>
+<p>Hello from header-blank</p>
--- wp-content/themes/understrap/index.php
+++ wp-content/themes/understrap/index.php
@@ -1,74 +1,26 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Understrap
- */
 
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-get_header();
-
-$container = get_theme_mod( 'understrap_container_type' );
+if (is_front_page()) {
+	is_user_logged_in() ? get_header('logged') : get_header('blank');
+}
 ?>
-
-<?php if ( is_front_page() && is_home() ) : ?>
-	<?php get_template_part( 'global-templates/hero' ); ?>
-<?php endif; ?>
-
-<div class="wrapper" id="index-wrapper">
-
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
-
-		<div class="row">
-
-			<?php
-			// Do the left sidebar check and open div#primary.
-			get_template_part( 'global-templates/left-sidebar-check' );
+<div class="container">
+	<div class="row">
+		<div class="col-4">1</div>
+		<div class="col-4">2</div>
+		<div class="col-4">3</div>
+		<div class="login-wp">
+			<?php 
+			wp_login_form();
 			?>
 
-			<main class="site-main" id="main">
+		</div>
+	</div>
 
-				<?php
-				if ( have_posts() ) {
-					// Start the Loop.
-					while ( have_posts() ) {
-						the_post();
+</div>
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
-					}
-				} else {
-					get_template_part( 'loop-templates/content', 'none' );
-				}
-				?>
 
-			</main>
-
-			<?php
-			// Display the pagination component.
-			understrap_pagination();
-
-			// Do the right sidebar check and close div#primary.
-			get_template_part( 'global-templates/right-sidebar-check' );
-			?>
-
-		</div><!-- .row -->
-
-	</div><!-- #content -->
-
-</div><!-- #index-wrapper -->
-
-<?php
+<?php 
 get_footer();
