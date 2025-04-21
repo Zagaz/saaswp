8d9c0990c7f5bcc17adc0d1691d87849f2ce6b14
Andre <andre_ranulfo@hotmail.com>
2025-04-21 13:22:26 -0300

    Implement logout confirmation modal and update logout button functionality

--- wp-content/themes/understrap/header-logged.php
+++ wp-content/themes/understrap/header-logged.php
@@ -35,13 +35,49 @@ get_template_part('template-parts/header-common');
     <!-- Logout button -->
     <ul class="navbar-nav ms-auto">
       <li class="nav-item">
-        
-        <a class="nav-link" href="<?php echo wp_logout_url(home_url('/?p=dashboard')); ?>">
-        <i class="fa-solid fa-right-from-bracket"></i>  
+        <a class="nav-link" id="logout-bt" href="javascript:void(0);">
+          <i class="fa-solid fa-right-from-bracket"></i>  
           Logout
         </a>
       </li>
     </ul>
   </div>
 </div>
+
+<!-- Logout Confirmation Modal -->
+<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true" data-bs-backdrop="static">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="logoutModalLabel">Confirm Logout</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Do you really want to logout?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary" id="confirm-logout">Logout</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+  const logoutButton = document.getElementById('logout-bt');
+  const confirmLogoutButton = document.getElementById('confirm-logout');
+
+  // Show the modal when the logout button is clicked
+  logoutButton.addEventListener('click', function () {
+    const logoutModal = new bootstrap.Modal(document.getElementById('logoutModal'));
+    logoutModal.show();
+  });
+
+  // Handle the logout action when the confirm button is clicked
+  confirmLogoutButton.addEventListener('click', function () {
+    window.location.href = "<?php echo wp_logout_url(home_url('/')); ?>";
+  });
+});
+</script>
 </nav>
