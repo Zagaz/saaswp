71eff4155455128f7353e692a48df64d643c3863
Andre <andre_ranulfo@hotmail.com>
2025-04-22 08:22:14 -0300

    some styling on is_success

--- wp-content/themes/understrap/template-parts/tpl-is-success.php
+++ wp-content/themes/understrap/template-parts/tpl-is-success.php
@@ -10,73 +10,70 @@ if (is_user_logged_in()) {
     return;
 }
 
-$is_success = isset($_GET['a']) ;
+$is_success = isset($_GET['a']);
 
 
-if ( $is_success == 0 ){
+
+if ($is_success == 0) {
     wp_redirect('/?p=login');
     exit;
 }
 
-if ( $is_success ){
+if ($is_success) {
+    $icon = 'fa fa-check';
     $alert = 'alert-success';
-    $message =  'Successs! Please check your email to get your password.' ;
+    $message =  'Successs! Please check your email to get your password.';
 } else {
+    $icon = 'fa-solid fa-circle-exclamation';
     $alert = 'alert-danger';
-    $message = 'Error - Please try again.';
-
+    $message = 'Ooops! Something went wrong - Please try again.';
 }
-
-
 ?>
 <div class="container">
     <div class="row">
-        <div class="alert <?php
-                            echo esc_attr($alert);
-                            ?>" role="alert">
-            Successs! Please check your email to get your password.
-        </div>
-    </div>
-
-    
-<div class="container">
-    <div class="row">
-  
-
-        <div class="col-6 form-login">
-            <?php // RIGHT - Login ?>
-            <h2>Login</h2>
-            
-            <div class="login-form">
-                <?php 
-                if (function_exists('wp_login_form')) {
-                    $args = array(
-                        'echo'              => true,
-                        'redirect'          => home_url(),
-                        'form_id'           => 'loginform',
-                        'label_username'    => __('Username or Email Address'),
-                        'label_password'    => __('Password'),
-                        'label_remember'    => __('Remember Me'),
-                        'label_log_in'      => __('Log In'),
-                        'id_username'       => 'user_login',
-                        'id_password'       => 'user_pass',
-                        'id_remember'       => 'rememberme',
-                        'id_submit'         => 'wp-submit',
-                        'remember'          => true,
-                        'value_username'    => '',
-                        'value_remember'    => false,
-                        'form_class'        => 'form-group',
-                        'before'            => '<div class="mb-3">',
-                        'after'             => '</div>',
-                    );
-                    wp_login_form($args);
-                } else {
-                    echo '<p>Login functionality is currently unavailable.</p>';
-                }
-                ?>
+        <div class="col-12">
+            <div class="alert  <?php
+                                echo esc_attr($alert);
+                                ?>" role="alert">
+                <i class="fa <?php echo esc_attr($icon); ?>"></i> <?php echo esc_html($message); ?>
             </div>
         </div>
     </div>
-</div>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-6 form-login">
+                <?php // RIGHT - Login 
+                ?>
+                <h2>Login</h2>
+                <div class="login-form">
+                    <?php
+                    if (function_exists('wp_login_form')) {
+                        $args = array(
+                            'echo'              => true,
+                            'redirect'          => home_url(),
+                            'form_id'           => 'loginform',
+                            'label_username'    => __('Username or Email Address'),
+                            'label_password'    => __('Password'),
+                            'label_remember'    => __('Remember Me'),
+                            'label_log_in'      => __('Log In'),
+                            'id_username'       => 'user_login',
+                            'id_password'       => 'user_pass',
+                            'id_remember'       => 'rememberme',
+                            'id_submit'         => 'wp-submit',
+                            'remember'          => true,
+                            'value_username'    => '',
+                            'value_remember'    => false,
+                            'form_class'        => 'form-group',
+                            'before'            => '<div class="mb-3">',
+                            'after'             => '</div>',
+                        );
+                        wp_login_form($args);
+                    } else {
+                        echo '<p>Login functionality is currently unavailable.</p>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
