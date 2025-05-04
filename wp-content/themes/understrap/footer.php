c4ad8b31f338de26ac8f904ee8cb8f37d5f144e2
Andre <andre_ranulfo@hotmail.com>
2025-05-04 18:50:01 -0300

    Footer

--- wp-content/themes/understrap/footer.php
+++ wp-content/themes/understrap/footer.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="fixed-bottom bg-light z-3">
     <?php wp_footer(); ?>
     <?php get_template_part('template-parts/tpl-stack'); ?>
 </footer>
--- wp-content/themes/understrap/template-parts/tpl-stack.php
+++ wp-content/themes/understrap/template-parts/tpl-stack.php
@@ -1,7 +1,7 @@
 <div class="container-fluid bg-secondary ">
   
        
-            <div class="col d-flex flex-row justify-content-between text-light p-2">
+            <div class="col d-flex flex-row justify-content-around text-light p-2" >
                 <h3> <i class="fa-brands fa-wordpress-simple"></i> </h3>
                 <h3> <i class="fa-brands fa-php"></i> </h3>
                 <h3><i class="fa-brands fa-css3-alt"></i></h3>
@@ -10,14 +10,9 @@
                 <h3>     <i class="fa-brands fa-bootstrap"></i>    </h3>
                 <h3>     <i class="fa-brands fa-linux"></i>    </h3>
                 <h3>    <i class="fa-brands fa-github"></i>     </h3>
-                <h3>         </h3>
-                <h3>         </h3>
-            </div>
-            <div class="col">
-            </div>
-            <div class="col">
                 
             </div>
+         
        
    
 </div>
